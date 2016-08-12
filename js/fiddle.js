204b639c6aed59ce217e0089a3e48098f760198b
Brandon Smith <basmith@gmail.com>
2016-08-12 11:08:56 -0400

    Working example with thumbnails.

--- js/fiddle.js
+++ js/fiddle.js
@@ -1,8 +1,59 @@
-var htmlEditor = document.getElementById("htmlEditor");
-var htmlPreview = document.getElementById("htmlPreview");
-var previewButton = document.getElementById("previewButton");
-var saveButton = document.getElementById("saveButton");
+$(document).ready(function () {
+    var htmlEditor = document.getElementById("htmlEditor");
+    var htmlPreview = document.getElementById("htmlPreview");
+    var previewButton = document.getElementById("previewButton");
+    var saveButton = document.getElementById("saveButton");
 
-previewButton.onclick = function() {
-  htmlPreview.innerHTML = htmlEditor.value;
-};
+    function improveContent(container) {
+        linker.getLinks(container, createLink, done);
+
+        function createLink(link) {
+
+            var word = link.word;
+            var results = link.results;
+
+            if (word === "") {
+                return;
+            }
+
+            if (results.length === 0) {
+                return;
+            }
+
+            var result = results[0];
+
+            try {
+                var regex = RegExp(word, 'gi');
+            } catch (e) {
+                console.error("Invalid regex: " + e);
+                return;
+            }
+
+            findAndReplaceDOMText(container, { // Defined in lib/jquery/plugins/jquery.ba-replacetext.min.js
+                find: regex,
+                replace: function (portion, match) {
+
+                    var span = document.createElement('span');
+                    span.innerHTML = portion.text;
+
+                    var img = document.createElement('img');
+                    img.src = linker.baseURL + result.thumbnail_url;
+
+                    span.appendChild(img);
+
+                    return span;
+                },
+                forceContext: findAndReplaceDOMText.NON_INLINE_PROSE
+            });
+        }
+
+        function done() {
+            console.log("Finito!");
+        }
+    }
+
+    previewButton.onclick = function() {
+        htmlPreview.innerHTML = htmlEditor.value;
+        improveContent(htmlPreview);
+    };
+});
