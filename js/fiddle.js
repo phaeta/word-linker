a2744c7f31090aa6527dedf2cadb2a7b2e40ebe4
Brandon Smith <basmith@gmail.com>
2016-08-12 15:00:11 -0400

    Final demonstration.

--- js/fiddle.js
+++ js/fiddle.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     var htmlPreview = document.getElementById("htmlPreview");
     var previewButton = document.getElementById("previewButton");
     var saveButton = document.getElementById("saveButton");
+    var linkCount = 0;
 
     function improveContent(container) {
         linker.getLinks(container, createLink, done);
@@ -11,7 +12,7 @@ $(document).ready(function () {
 
             var word = link.word;
             var results = link.results;
-            var linkCount = 0;
+            
 
             if (word === "") {
                 return;
@@ -33,7 +34,6 @@ $(document).ready(function () {
             findAndReplaceDOMText(container, { // Defined in lib/jquery/plugins/jquery.ba-replacetext.min.js
                 find: regex,
                 replace: function (portion, match) {
-                    // TODO: highlight words
                     // TODO: separate word from image
                     // TODO: embed
 
@@ -41,19 +41,31 @@ $(document).ready(function () {
                     span.innerHTML = portion.text;
                     span.className = 'highlight';
 
-                    var img = document.createElement('img');
-                    img.src = linker.baseURL + result.thumbnail_url;
+                    //var img = document.createElement('img');
+                    //img.src = linker.baseURL + result.thumbnail_url;
 
-                    span.appendChild(img);
-                    img.width = "200";
+                    //span.appendChild(img);
+                    //img.width = "200";
+
+                    // ui hack for thumbnail awesomeness - extremely brittle
+                    function fixURL(url) {
+                        var regex = new RegExp("production/maleAdult/(.+).json");
+                        return url.replace(regex, "$1");
+                    }
+
+                    var iframe = document.createElement('iframe');
+                    iframe.src = linker.baseURL + fixURL(result.embed_url) + "&ui-nav=false&ui-share=false&ui-info=false";
+                    iframe.width = "200";
+                    iframe.height = "200";
 
                     if (linkCount % 2 == 0) {
-                        img.className = 'fl_right';
+                        iframe.className = 'fl_right';
                     } else {
-                        img.className = 'fl_left';
+                        iframe.className = 'fl_left';
                     }
                     linkCount += 1;
 
+                    span.appendChild(iframe);
                     return span;
                 },
                 forceContext: findAndReplaceDOMText.NON_INLINE_PROSE
